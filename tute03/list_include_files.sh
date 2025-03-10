2a9efc4c630ef3b9b72cdcfa846e78b164655b1c
winnieee-chan <winnie.chichan@gmail.com>
2025-03-10 12:33:08 +1100

    Add tute04

--- tute03/list_include_files.sh
+++ tute03/list_include_files.sh
@@ -1,8 +1,25 @@
 #!/bin/dash
 
+
 for file in *.c
 do
     echo "$file includes:"
     sed -En 's/^#include ["<](.*)[">]$/    \1/p' "$file" 
 done
 
+# W09B:
+# for file in *.c; do
+#     # echo "$file"
+#     cat "$file" | sed -En 's/^.* [<](.*)[>]$/\1/p'
+# done
+
+# H09A
+# for file in *.c; do
+#     echo "$file"
+#     grep -E '^#include' "$file" | sed -E 's/^#include [<"](.*)[>"]/\1/'
+# done
+
+# F13B
+# for name in *.c; do
+#     sed -En 's/#include [\<"](.*)[\>"]/\1/gp' "$name"
+# done
