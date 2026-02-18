6d236429b32f3b31611dc99c84abb23e8d731f81
winnieee-chan <winnie.chichan@gmail.com>
2026-02-18 20:27:17 +1100

    Add initial program structure and print messages

--- tute01/program.c
+++ tute01/program.c
@@ -1,1 +1,15 @@
-int main
+#include <stdio.h>    // trailing whitespace here:    
+
+#define MSG "Hello from a macro!"	 	
+
+int main(void) {
+    printf("%s\n", MSG);
+
+    // This line contains a # but is NOT a preprocessor command:
+    printf("This is not a directive: # just a character in a string\n");
+
+    // Another non-directive # (comment text):
+    // # not a directive because it’s inside a comment
+
+    return 0;     
+}
